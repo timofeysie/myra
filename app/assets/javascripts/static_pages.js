5a4c0db56fa5e1096139d4fdf8f7205329ecf7b2
Timothy Curchod <timofeyc@hotmail.com>
2014-10-30 08:58:11 +0000

    Changed the link for sales and slowed the fade-in a bit.

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,7 +2,7 @@ $(function(){
 // hide image, pause then fade in image
       $('img').hide();
       setInterval(function(){
-        $('img').fadeIn(5000);}, 1000);
+        $('img').fadeIn(4000);}, 1000);
 });
   
 $('.fadeOut').click(function() {
